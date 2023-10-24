62875bda1c964b7b0e6de66a63074d6e3e83538b
Fenwick <a.povse1@gmail.com>
2023-10-24 17:12:44 -0400

    Limited Drive Constraints Power Limit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/DriveConstants.java
@@ -65,7 +65,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = MAX_RPM/60 * GEAR_RATIO * WHEEL_RADIUS * 2 * 3.14;
+    public static double LIMIT = 0.8;
+    public static double MAX_VEL = MAX_RPM/60 * GEAR_RATIO * WHEEL_RADIUS * 2 * 3.14 * LIMIT;
     public static double MAX_ACCEL = 38.110287416570166;
     public static double MAX_ANG_VEL = Math.toRadians(138.19991297468354);
     public static double MAX_ANG_ACCEL = Math.toRadians(138.19991297468354);
